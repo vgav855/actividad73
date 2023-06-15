d5b677d905f434eb28a3a87587e9e2b8b1604db6
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-06-15 09:03:19 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorStop(maqueen.Motors.M2)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorStop(maqueen.Motors.M1)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+})
